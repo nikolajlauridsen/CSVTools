134a1ce453f70537b0ed0061795b07ad2853e207
Molex <simplesleeptimer@gmail.com>
2019-03-18 13:39:15 +0100

    Add project files.

---
+++ CSVTools/Cell.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVTools
+{
+    internal class Cell : IComparable<Cell>
+    {
+        public int Position;
+        private object _data;
+
+        public Cell(object data, int position)
+        {
+            _data = data;
+            Position = position;
+        }
+
+        public object GetData()
+        {
+            return _data;
+        }
+
+        public void SetContent(object data)
+        {
+            _data = data;
+        }
+
+        public override string ToString()
+        {
+            return _data.ToString();
+        }
+
+        public int CompareTo(Cell other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            return Position.CompareTo(other.Position);
+        }
+    }
+}
---
+++ CSVTools/Row.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVTools
+{
+    internal class Row
+    {
+        /// <summary>
+        /// The Y position of the row
+        /// </summary>
+        public int Position;
+
+        /// <summary>
+        /// The width of the row
+        /// </summary>
+        public int Width => Cells[Cells.Count - 1].Position;
+        
+        /// <summary>
+        /// A sorted copy of the cells (readonly)
+        /// </summary>
+        public List<Cell> Cells
+        {
+            get
+            {
+                List<Cell> returnList = new List<Cell>(_row);
+                returnList.Sort();
+                return returnList;
+            }
+        }
+        private List<Cell> _row = new List<Cell>();
+
+        /// <summary>
+        /// Insert an object into the cell at the x position of the column.
+        /// Overwrites the data in the cell if it already exists
+        /// </summary>
+        /// <param name="data">Data to be inserted</param>
+        /// <param name="x">The x position of the cell within the row</param>
+        public void Insert(object data, int x)
+        {
+            try
+            {
+                Cell targetCell = _row.Find(cell => cell.Position == x);
+                targetCell.SetContent(data);
+            }
+            catch (ArgumentNullException)
+            {
+                Cells.Add(new Cell(data, x));
+            }
+        }
+
+    }
+}
