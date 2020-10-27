321fca32a8cd497385dab2250b6bb5c2c53f7d0f
Mole <nikolajlauridsen@protonmail.ch>
2020-10-27 16:24:18 +0100

    Add test to test the new SpecifyColumnDelimiterInFile

---
+++ CSVTest/CSVFormatterTests.cs
@@ -0,0 +1,42 @@
+using CSVTools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSVTest
+{
+    [TestClass]
+    public class CSVFormatterTests
+    {
+        [TestMethod]
+        public void EvenTableWithSep()
+        {
+            Table table = new Table();
+            table[1, 1] = 1;
+            table[2, 1] = 2;
+            table[1, 2] = 3;
+            table[2, 2] = 4;
+            
+            CSVFormatter sut = new CSVFormatter(table);
+            
+            string expected = "sep=;\n1;2\n3;4\n";
+            string actual = sut.GetCSV(table);
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void EvenTableWithNoSep()
+        {
+            Table table = new Table();
+            table.SpecifyColumnDelimiterInFile = false;
+            table[1, 1] = 1;
+            table[2, 1] = 2;
+            table[1, 2] = 3;
+            table[2, 2] = 4;
+            
+            CSVFormatter sut = new CSVFormatter(table);
+
+            string expected = "1;2\n3;4\n";
+            string actual = sut.GetCSV(table);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- CSVTools/CSVFormatter.cs
+++ CSVTools/CSVFormatter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -37,7 +38,7 @@ namespace CSVTools
 
             if (table.SpecifyColumnDelimiterInFile)
             {
-                tableBuilder.AppendLine($"sep={ColumnDelimiter}");
+                tableBuilder.Append($"sep={ColumnDelimiter}{RowDelimiter}");
             }
 
             foreach (Row row in table.Rows)
